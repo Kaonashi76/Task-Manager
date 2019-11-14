019a91cf8b422b59b06b0f135db7f53b2d5a4ab1
Max Glotov <glotovmaks97@gmail.com>
2019-11-14 20:30:55 +0300

    change css, html & refactoring js

--- js/script.js
+++ js/script.js
@@ -1,72 +1,89 @@
-let noteCounter   = 3;
-let columnCounter = 4;
-let draggedNote  = null;
+let noteCounter   = 1;
+let columnCounter = 1;
+let draggedNote   = null;
 
 const buttonAddColumn = document.querySelector('[data-action-addColumn]');
 const headerElement   = document.querySelectorAll('.column-header');
 const note            = document.querySelectorAll('.note');
 const column          = document.querySelectorAll('.column');
 
-note
-  .forEach(ChangeNote);
+//Create a new column && add it to the DOM
+function CreateColumn(){
+  const newColumn = document.createElement('div');
+  newColumn.classList.add('column');
+  newColumn.setAttribute('data-column-id', columnCounter);      
+  columnCounter++;
 
-headerElement
-  .forEach(ChangeHeaderColumn);
-
-column
-  .forEach(CreateColumnElement);
-
-
-  buttonAddColumn.addEventListener('click', function (event){
-    const newColumn = document.createElement('div');
-    newColumn.classList.add('column');
-    newColumn.setAttribute('draggable', 'true');
-    newColumn.setAttribute('data-column-id', columnCounter);      
-    columnCounter++;
-
-    newColumn.innerHTML += `<p class="column-header"> </p>
-                              <div data-notes></div>
-                              <p class="column-footer">
-                                <span data-action-addNote class="action">+ Добавить карточку</span>
-                              </p>`
+  newColumn.innerHTML += `<p class="column-header"> </p>
+                            <div data-notes></div>
+                          <p class="column-footer">
+                            <button data-action-addNote class="action">Добавить карточку</button>
+                          </p>`
     document.querySelector('.columns').append(newColumn);  
-   
+    return newColumn;
+}
+
+buttonAddColumn.addEventListener('click', function (event){
+
+    newColumn = CreateColumn();
+
+    //focus column heading
     newColumn.querySelector('.column-header').setAttribute('contenteditable','true');
     newColumn.querySelector('.column-header').focus();
 
+    //add event create new note
     CreateColumnElement(newColumn);
+
+    //column header handler
     ChangeHeaderColumn(newColumn.querySelector('.column-header'));
 
   });
 
+//If you have notes, then add event change notes
+note
+  .forEach(ChangeNote);
 
+//If you have columns, then add event rename column heading
+headerElement
+  .forEach(ChangeHeaderColumn);
+
+// If you have notes, then add event create new note
+column
+  .forEach(CreateColumnElement);
+
+ //create new note && add it to the DOM
+ function createNote(columnElement){
+  const newNote = document.createElement('div');
+  newNote.classList.add('note');
+  newNote.setAttribute('draggable', 'true');
+  newNote.setAttribute('data-note-id', noteCounter); 
+  newNote.setAttribute('contenteditable','true');     
+  noteCounter++;
+
+  columnElement.querySelector('[data-notes]').append(newNote); 
+
+  return newNote
+}
 
 function CreateColumnElement(columnElement){
   const buttonAddNote = columnElement.querySelector('[data-action-addNote]');
-    
+
   buttonAddNote.addEventListener('click', function (event) {
     
-    const newNote = document.createElement('div');
-    newNote.classList.add('note');
-    newNote.setAttribute('draggable', 'true');
-    newNote.setAttribute('data-note-id', noteCounter); 
-    newNote.setAttribute('contenteditable','true');     
-    noteCounter++;
-
-    columnElement.querySelector('[data-notes]').append(newNote); 
+    //create new note && add it to the DOM
+    newNote = createNote(columnElement);
+   
+    //autofocus new note
     newNote.focus();
+
+    //note text change handler
     ChangeNote(newNote);
-    
+    DragNotes(noteElement);
     
   });
-  columnElement.addEventListener('dragover', function(event){
-    event.preventDefault();
-  })
-  columnElement.addEventListener('drop', function(event){
-    if (draggedNote){
-      columnElement.querySelector('[data-notes]').append(draggedNote);
-    }
-  })
+ 
+  //event handlers for dragging a note into an empty column
+  DragNotesToEmptyColumn(columnElement);
   
 }
 
@@ -77,10 +94,13 @@ function ChangeNote(noteElement){
   });
   noteElement.addEventListener('blur', function(event){
     noteElement.removeAttribute('contenteditable');
+    
     if (noteElement.innerText.trim() === ''){
       noteElement.remove();
     }
+
   });
+ 
   DragNotes(noteElement);
 }
 
@@ -98,6 +118,18 @@ function ChangeHeaderColumn(headerElement){
   
 }
 
+function DragNotesToEmptyColumn (columnElement){
+  columnElement.addEventListener('dragover', function(event){
+    event.preventDefault();
+  })
+  
+  columnElement.addEventListener('drop', function(event){
+    if (draggedNote){
+      columnElement.querySelector('[data-notes]').append(draggedNote);
+    }
+  })
+}
+ 
 function DragNotes(noteElement){
   noteElement.addEventListener('dragstart', DragStartNoteHandler);
   noteElement.addEventListener('dragend',   DragEndNoteHandler);
