de7632011fdc9fe3a43bf4f22f974d555c9ccd91
Max Glotov <glotovmaks97@gmail.com>
2019-11-14 18:52:53 +0300

    add drag'n'drop note

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-let noteCounter = 3;
+let noteCounter   = 3;
 let columnCounter = 4;
+let draggedNote  = null;
 
 const buttonAddColumn = document.querySelector('[data-action-addColumn]');
 const headerElement   = document.querySelectorAll('.column-header');
@@ -23,15 +24,19 @@ column
     newColumn.setAttribute('data-column-id', columnCounter);      
     columnCounter++;
 
-    newColumn.innerHTML += `<p class="column-header">В плане</p>
+    newColumn.innerHTML += `<p class="column-header"> </p>
                               <div data-notes></div>
                               <p class="column-footer">
                                 <span data-action-addNote class="action">+ Добавить карточку</span>
                               </p>`
     document.querySelector('.columns').append(newColumn);  
+   
+    newColumn.querySelector('.column-header').setAttribute('contenteditable','true');
+    newColumn.querySelector('.column-header').focus();
 
     CreateColumnElement(newColumn);
     ChangeHeaderColumn(newColumn.querySelector('.column-header'));
+
   });
 
 
@@ -44,23 +49,39 @@ function CreateColumnElement(columnElement){
     const newNote = document.createElement('div');
     newNote.classList.add('note');
     newNote.setAttribute('draggable', 'true');
-    newNote.setAttribute('data-note-id', noteCounter);      
+    newNote.setAttribute('data-note-id', noteCounter); 
+    newNote.setAttribute('contenteditable','true');     
     noteCounter++;
 
     columnElement.querySelector('[data-notes]').append(newNote); 
+    newNote.focus();
     ChangeNote(newNote);
+    
+    
   });
+  columnElement.addEventListener('dragover', function(event){
+    event.preventDefault();
+  })
+  columnElement.addEventListener('drop', function(event){
+    if (draggedNote){
+      columnElement.querySelector('[data-notes]').append(draggedNote);
+    }
+  })
+  
 }
 
 function ChangeNote(noteElement){
   noteElement.addEventListener('dblclick', function(event){
     noteElement.setAttribute('contenteditable','true');
     noteElement.focus();
-  })
+  });
   noteElement.addEventListener('blur', function(event){
     noteElement.removeAttribute('contenteditable');
-  }
-  );
+    if (noteElement.innerText.trim() === ''){
+      noteElement.remove();
+    }
+  });
+  DragNotes(noteElement);
 }
 
 function ChangeHeaderColumn(headerElement){
@@ -70,5 +91,77 @@ function ChangeHeaderColumn(headerElement){
   })
   headerElement.addEventListener('blur', function(event){
     headerElement.removeAttribute('contenteditable');
+    if (headerElement.innerText.trim() === ''){
+     headerElement.parentElement.remove();
+    }
   });
+  
+}
+
+function DragNotes(noteElement){
+  noteElement.addEventListener('dragstart', DragStartNoteHandler);
+  noteElement.addEventListener('dragend',   DragEndNoteHandler);
+  noteElement.addEventListener('dragenter', DragEnterNoteHandler);
+  noteElement.addEventListener('dragover',  DragOverNoteHandler);
+  noteElement.addEventListener('dragleave', DragLeaveNoteHandler);
+  noteElement.addEventListener('drop',  DragDropNoteHandler);
+}
+
+function DragStartNoteHandler (event){
+  draggedNote = this; 
+
+  this.classList.add('dragged');
+  event.stopPropagation();
+}
+
+function DragEndNoteHandler (event){
+  draggedNote = null;
+  this.classList.remove('dragged'); 
+  
+  document
+    .querySelectorAll('.note')
+    .forEach(element => element.classList.remove('under'));
+}
+
+function DragEnterNoteHandler (event){
+  if (this === draggedNote){
+    return
+  }
+  this.classList.add('under');
+}
+
+function DragOverNoteHandler (event){
+  event.preventDefault();
+
+  if (this === draggedNote){
+    return
+  }
+}
+
+function DragLeaveNoteHandler (event){
+  if (this === draggedNote){
+    return
+  }
+  this.classList.remove('under');
+}
+
+function DragDropNoteHandler (event){
+  event.stopPropagation();
+  if (this === draggedNote){
+    return
+  } 
+  if(this.parentElement === draggedNote.parentElement){
+    const notes = Array.from(this.parentElement.querySelectorAll('.note'));
+    const indexA = notes.indexOf(this)
+    const indexB = notes.indexOf(draggedNote)
+    if (indexA < indexB){
+      this.parentElement.insertBefore(draggedNote,this)
+    }
+    else{
+      this.parentElement.insertBefore(draggedNote,this.nextElementSibling)
+    }
+    
+  }else{
+    this.parentElement.insertBefore(draggedNote,this)
+  }
 }
